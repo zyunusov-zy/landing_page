f5a65c03c243bf6448a4961f8cbd9d3c1efc348c
Zafarbek Yunusov <zyunusov30@gmail.com>
2025-04-23 13:11:36 +0500

    change footer cross platform styling

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,11 +3,11 @@ import { Instagram, Facebook, Twitter, Youtube } from "lucide-react";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#2d2e40] text-white py-10 px-6 md:px-20 flex justify-around items-center flex-wrap text-base gap-4">
+    <footer className="bg-[#2d2e40] text-white py-10 px-6 md:px-20 flex justify-around items-center flex-wrap text-base md:flex-row flex-col gap-6">
       {/* logo and social media */}
       <div className="flex flex-col gap-4">
         <img src="/logo.svg" alt="Logo" className="invert brightness-0"/>
-        <div className="flex gap-2">
+        <div className="flex gap-2 items-center justify-center">
           <Instagram className="w-6 h-6 text-white hover:text-green-400 transition-colors duration-200" />
           <Facebook className="w-6 h-6 text-white hover:text-green-400 transition-colors duration-200" />
           <Twitter className="w-6 h-6 text-white hover:text-green-400 transition-colors duration-200" />
@@ -16,8 +16,8 @@ const Footer = () => {
       </div>
 
       {/* Navbar */}
-      <nav className=" gap-20 text-gray-300 font-semibold hidden md:flex">
-        <ul className="flex flex-col gap-2">
+      <nav className=" md:gap-20 gap-2 text-gray-300 font-semibold flex md:flex-row flex-col">
+        <ul className="flex flex-col gap-2 justify-center items-center md:items-start">
           <li>
             <a href="" className="hover:text-green-300 hover:underline transition-all">
               About Us
@@ -34,7 +34,7 @@ const Footer = () => {
             </a>
           </li>
         </ul>
-        <ul className="flex flex-col gap-2">
+        <ul className="flex flex-col gap-2 justify-center items-center md:items-start">
           <li>
             <a href="" className="hover:text-green-300 hover:underline transition-all">
               Careers
@@ -54,7 +54,7 @@ const Footer = () => {
       </nav>
 
       {/* Button */}
-      <div className="flex flex-col gap-5 justify-center items-end">
+      <div className="flex flex-col gap-5 justify-center md:items-end ">
         <button className="bg-gradient-to-r from-green-400 to-blue-500 text-white md:px-6 md:py-2 md:text-base px-4 py-2 text-sm rounded-full font-semibold shadow-md hover:from-green-500 hover:to-blue-600 transition-all">
           Request Invite
         </button>
